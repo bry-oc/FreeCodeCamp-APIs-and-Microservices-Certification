435a271723e2d3124690b2df8dbe227605487918
bryan-ocbina <bryan.ocbina@knights.ucf.edu>
2021-05-20 16:10:01 -0400

    Added comments.  Added root logger for request.  Added GET requests to send current time.  Added GET requests to echo path

--- Tutorials/Basic Node and Express/myApp.js
+++ Tutorials/Basic Node and Express/myApp.js
@@ -4,7 +4,7 @@ var dotenv = require('dotenv').config();
 
 console.log("Hello World");
 
-//set static elements from public path
+//set static elements from public path with middleware
 let relativePath = '/public';
 let absolutePath = __dirname + relativePath;
 
@@ -12,13 +12,19 @@ console.log(absolutePath);
 
 app.use(relativePath, express.static(absolutePath));
 
+//create a logger for requests
+app.use('/', function(req, res, next){
+    console.log(req.method + " " + req.path + " - " + req.ip);
+    next();
+});
 /*
+//root path will send a response with string "Hello Express"
 app.get('/',  function (req, res) {
     res.send('Hello Express');
 });
 */
 
-//root path will load index.html in views path
+//root path will load index.html in views path with GET request
 let file = '/index.html';
 relativePath = '/views';
 absolutePath = __dirname + relativePath + file;
@@ -29,7 +35,7 @@ app.get('/', function(req, res) {
     res.sendFile(absolutePath);
 });
 
-//send a json response depending the the hidden .env variable
+//send a json response depending on the hidden .env variable with GET request
 relativePath = '/json';
 let message_text = "Hello json"
 app.get(relativePath, function(req, res) {
@@ -38,10 +44,20 @@ app.get(relativePath, function(req, res) {
     } else {
         res.json({message: message_text});
     }
-    
 });
 
+//send the current time in a GET request at the '/now' path with a middleware function to set the time
+app.get('/now', function(req, res, next){
+    req.time = new Date().toString();
+    next();
+}, function(req, res){
+    res.json({time: req.time});
+});
 
+//send an echo of the word in '/:word/echo' path with a GET request
+app.get('/:word/echo', function(req, res){
+    res.json({echo: req.params.word})
+});
 
 
 
