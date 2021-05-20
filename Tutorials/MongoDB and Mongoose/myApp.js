4bc5d8a00604bd371e4a81df05220de9179af6d1
bryan-ocbina <bryan.ocbina@knights.ucf.edu>
2021-05-20 18:37:49 -0400

    Add connection to mongodb

--- Tutorials/MongoDB and Mongoose/myApp.js
+++ Tutorials/MongoDB and Mongoose/myApp.js
@@ -1,5 +1,7 @@
-require('dotenv').config();
+var mongoose = require('mongoose');
+var dotenv = require('dotenv').config();
 
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
 let Person;
 
@@ -50,6 +52,8 @@ const queryChain = (done) => {
 
   done(null /*, data*/);
 };
+
+
 
 /** **Well Done !!**
 /* You completed these challenges, let's go celebrate !
