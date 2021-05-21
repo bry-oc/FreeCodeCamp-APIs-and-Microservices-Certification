a5f00f9f3f3f499093cd72fe54d7d5548d0886cd
bryan-ocbina <bryan.ocbina@knights.ucf.edu>
2021-05-21 14:05:55 -0400

    Added lookup to find person by mongodb assigned id

--- Tutorials/MongoDB and Mongoose/myApp.js
+++ Tutorials/MongoDB and Mongoose/myApp.js
@@ -49,7 +49,12 @@ const findOneByFood = (food, done) => {
 };
 
 const findPersonById = (personId, done) => {
-  done(null /*, data*/);
+  Person.findById({_id: personId}, function(err, data) {
+    if(err){
+      return console.error(err)
+    }
+    done(null , data);
+  })
 };
 
 const findEditThenSave = (personId, done) => {
