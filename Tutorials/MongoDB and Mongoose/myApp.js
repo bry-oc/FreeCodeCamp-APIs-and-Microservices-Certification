47251d4b050f0b291cf8e1fc78599b4cf0cf63d4
bryan-ocbina <bryan.ocbina@knights.ucf.edu>
2021-05-21 14:02:41 -0400

    Added lookup to find one person based on favorite foods

--- Tutorials/MongoDB and Mongoose/myApp.js
+++ Tutorials/MongoDB and Mongoose/myApp.js
@@ -35,12 +35,17 @@ const findPeopleByName = (personName, done) => {
     if(err) {
       return console.error(err);
     }
-     done(null , data);
+    done(null , data);
   })
 };
 
 const findOneByFood = (food, done) => {
-  done(null /*, data*/);
+  Person.findOne({favoriteFoods: food}, function(err,data) {
+    if(err) {
+      return console.error(err)
+    }
+    done(null, data);
+  })
 };
 
 const findPersonById = (personId, done) => {
